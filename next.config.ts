59ff9e50839479b3da400df1714edf08e68793ef
ZoeYoon <theky8690@gmail.com>
2026-09-20 18:59:20 +0900

    feat: vanilla extract설치 및 셋팅

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
 };
 
-export default nextConfig;
+export default withVanillaExtract;
